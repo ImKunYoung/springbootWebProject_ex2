ff3595cd793943d407396e3fbd08bea57204038f
ImKunYoung <lim8667@gmail.com>
2022-07-02 04:43:17 +0900

    entity 클래스

--- src/main/java/com/example/ex2/entity/Memo.java
+++ src/main/java/com/example/ex2/entity/Memo.java
@@ -1,17 +1,22 @@
 package com.example.ex2.entity;
 
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name="tbl_memo")
 @ToString
+@Getter
+@Builder // 객체 생성
+@AllArgsConstructor // Builder 사용 시 컴파일 에러 방지
+@NoArgsConstructor // Builder 사용 시 컴파일 에러 방지
 public class Memo {
-
-
     @Id // pk
     @GeneratedValue(strategy = GenerationType.IDENTITY) // default value IDENTITY
     private Long mno;
 
+//    @Column(columnDefinition = "varchar(255) default 'yes'")
+    @Column(length = 200, nullable = false)
+    private String memoText;
 }
