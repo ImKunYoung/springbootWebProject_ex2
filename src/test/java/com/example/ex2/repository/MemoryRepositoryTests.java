d68ec504d074b3f6b7e428091455a197e131e8a9
ImKunYoung <lim8667@gmail.com>
2022-07-02 08:20:48 +0900

    조회 작업 테스트 findById(), getReferenceById()

--- src/test/java/com/example/ex2/repository/MemoryRepositoryTests.java
+++ src/test/java/com/example/ex2/repository/MemoryRepositoryTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -20,11 +22,40 @@ public class MemoryRepositoryTests {
     }
 
     @Test
-    public void testInsertDummies(){
-        IntStream.rangeClosed(1, 100).forEach(i->{
-            Memo memo = Memo.builder().memoText("Sample..."+i).build();
+    public void testInsertDummies() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Memo memo = Memo.builder().memoText("Sample..." + i).build();
             memoRepository.save(memo);
         });
     }
+
+    @Test
+    public void testSelect() { // 데이터베이스 먼저 이용
+        // 데이터베이스에 존재하는 mno
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+        System.out.println("========================================");
+
+        // result가 존재하면 print memo
+        if(result.isPresent()){
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
+    }
+
+    @Transactional
+    @Test
+    public void testSelect2() { // 데이터베이스 필요한 순간
+        // 데이터베이스에 존재하는 mno
+        Long mno = 100L;
+
+        Memo memo = memoRepository.getReferenceById(mno);
+
+        System.out.println("========================================");
+
+        System.out.println(memo);
+    }
+
 
 }
