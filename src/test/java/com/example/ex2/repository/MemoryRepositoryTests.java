bec9ff02a314866bbffac0eac1ee07ab6ec16ebc
ImKunYoung <lim8667@gmail.com>
2022-07-02 16:04:22 +0900

    페이징 처리

--- src/test/java/com/example/ex2/repository/MemoryRepositoryTests.java
+++ src/test/java/com/example/ex2/repository/MemoryRepositoryTests.java
@@ -1,10 +1,12 @@
 package com.example.ex2.repository;
 
-
 import com.example.ex2.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
@@ -18,6 +20,7 @@ public class MemoryRepositoryTests {
 
     @Test
     public void testClass() { System.out.println(memoRepository.getClass().getName()); }
+
     /* 등록 작업 테스트 */
     @Test
     public void testInsertDummies() {
@@ -26,25 +29,26 @@ public class MemoryRepositoryTests {
             memoRepository.save(memo);
         });
     }
-    /* 조회 작업 테스트 1 */
+
+    /* 조회 작업 테스트 1 -  데이터베이스 먼저 이용 */
     @Test
-    public void testSelect() { // 데이터베이스 먼저 이용
+    public void testSelect() {
         // 데이터베이스에 존재하는 mno
         Long mno = 100L;
 
         Optional<Memo> result = memoRepository.findById(mno);
+
         System.out.println("========================================");
 
-        // result가 존재하면 print memo
-        if(result.isPresent()){
+        if(result.isPresent()){ // result 가 존재하면 print memo
             Memo memo = result.get();
             System.out.println(memo);
         }
     }
-    /* 조회 작업 테스트 2 */
+    /* 조회 작업 테스트 2 - 데이터베이스 필요한 순간 */
     @Transactional
     @Test
-    public void testSelect2() { // 데이터베이스 필요한 순간
+    public void testSelect2() {
         // 데이터베이스에 존재하는 mno
         Long mno = 100L;
 
@@ -67,6 +71,33 @@ public class MemoryRepositoryTests {
         memoRepository.deleteById(mno);
     }
 
+    /* 페이징 처리 */
+    @Test
+    public void testPageDefault() { // 1페이지 10개
+        Pageable pageable = PageRequest.of(0, 10);
 
+        Page<Memo> result = memoRepository.findAll(pageable);
 
+        System.out.println(result);
+
+        System.out.println("-----------------------------------------------");
+
+        System.out.println("Total Pages: "+result.getTotalPages()); // 총 몇 페이지
+
+        System.out.println("Total Count: "+result.getTotalElements()); // 전체 개수
+
+        System.out.println("Page Number: "+result.getNumber()); // 현재 페이지 번호 0부터 시작
+
+        System.out.println("Page Size: "+result.getSize()); // 페이지당 데이터 개수
+
+        System.out.println("has next page?: "+result.hasNext()); // 다음 페이지 존재 여부
+
+        System.out.println("first page?: "+result.isFirst()); // 시작 페이지(0) 여부
+
+        System.out.println("-----------------------------------------------");
+
+        for(Memo memo : result.getContent()){
+            System.out.println(memo);
+        }
+    }
 }
